ce7acf94b710ff79a741335b3734c1865d271b15
VictorGabrielQS <victor.git24@gmail.com>
2025-07-31 06:50:23 -0300

    Ternarios

---
+++ fundamentos/l_operadores/Unarios/DesafioUnario.java
@@ -0,0 +1,36 @@
+package Unarios;
+
+import java.util.Scanner;
+
+public class DesafioUnario {
+
+    static class Desafio1 {
+        public static void main(String[] args) {
+            
+            Scanner entrada = new Scanner(System.in);
+            
+            System.out.println("Digite o numero que deseja declementar : ");
+            int n = entrada.nextInt();
+
+       
+
+            while (n >= 2) {
+                System.out.println(n);
+                n--;
+            }  
+        }
+    }
+
+
+    static class Desafio2 {
+    public static void main(String[] args) {
+        
+        boolean luzLigada = false;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("\nLuz ligada ?%s" , luzLigada + " Vez%s" , i );
+        }
+    }
+        
+    }
+}
--- fundamentos/l_operadores/Unarios/Unario.java
+++ fundamentos/l_operadores/Unarios/Unario.java
@@ -2,9 +2,25 @@ package Unarios;
 
 public class Unario {
     public static void main(String[] args) {
+
+        int a = 1;
+        int b = 5;
+
+        a++;
+        b--;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        
         
 
-        int a = 1; 
+        int x = 10;
+
+        System.out.println(++x); // 11 (incrementa, depois usa)
+        System.out.println(x++); // 11 (usa, depois incrementa)
+        System.out.println(x); // 12 (valor atualizado)
+
     }
 
 }
