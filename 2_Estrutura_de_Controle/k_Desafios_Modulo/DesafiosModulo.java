3456bd65fa8142f351b8e3efcc9fa8cbd9019c37
VictorGabrielQS <victor.git24@gmail.com>
2025-08-19 16:27:54 -0300

    Desafio 3 Modulo Estrutura de controle

--- 2_Estrutura_de_Controle/k_Desafios_Modulo/DesafiosModulo.java
+++ 2_Estrutura_de_Controle/k_Desafios_Modulo/DesafiosModulo.java
@@ -9,7 +9,6 @@ public class DesafiosModulo {
      
         Scanner scanner = new Scanner(System.in);
         
-
         System.out.print("DIgite um numero : ");
         int numero = scanner.nextInt();
 
@@ -18,6 +17,55 @@ public class DesafiosModulo {
         }else{
         System.out.println("O numero não esta entre 10");
         }
+
+        if (numero % 2 == 0) {
+            System.out.println("E o numero e par ");
+            
+        }else{
+            System.out.println("O numero e Impar");
+        }
+
+    }
+    }
+
+
+
+    static class Desafio2 {
+    public static void main(String[] args) {
+     
+        int ano = 2100;
+
+        if ( (ano % 400 == 0) || (ano % 4 == 0 && ano % 100 != 0) ) {
+            System.out.println("O ano e bissexto ... ");
+            
+        }else{
+            System.out.println("O ano não e bissexto...");
+        }
+
+        
+    }  
+    }
+
+
+
+    static class Desafio3 {
+    public static void main(String[] args) {
+     
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite a primeira nota : ");
+        int nota1 = scanner.nextInt();
+        
+        
+        System.out.print("Digite a primeira nota : ");
+        int nota2 = scanner.nextInt();
+
+
+        int media = (nota1 + nota2 ) / 2 ;
+
+        System.out.println("A media e : " + media);
+
+
     }
     }
 
