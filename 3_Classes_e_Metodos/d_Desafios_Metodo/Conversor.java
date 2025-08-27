20a7c44f4bdd480706a4e751969110c034179e54
VictorGabrielQS <victor.git24@gmail.com>
2025-08-27 07:13:51 -0300

    Desafio Metodo 2

---
+++ 3_Classes_e_Metodos/d_Desafios_Metodo/Conversor.java
@@ -0,0 +1,12 @@
+public class Conversor {
+
+    double valor;
+
+    double conversor (double valor){
+
+        double resultadoConversao = valor / 5.00;
+
+        return resultadoConversao;
+    }
+
+}
---
+++ 3_Classes_e_Metodos/d_Desafios_Metodo/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Desafio conversor de Real para Dolar
+        Conversor conversor1 = new Conversor();
+        System.out.println("Valor da conversão em dolar e : "+  conversor1.conversor(5.00) + " USD " +
+                "" +
+                "");
+
+    }
+}
