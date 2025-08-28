2c94bb4edb7e2cefca39724b08cf28ac696fff6c
VictorGabrielQS <victor.git24@gmail.com>
2025-08-28 16:25:50 -0300

    Construtor

---
+++ 3_Classes_e_Metodos/e_Construtor/Construtor.java
@@ -0,0 +1,8 @@
+public class Construtor {
+    public static void main(String[] args) {
+        ProdutoC produto1 = new ProdutoC("Notebook");
+
+
+        System.out.println(produto1.nome);
+    }
+}
