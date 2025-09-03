feb7480492e6c194fe46ab4bc8531d61c239b1b8
VictorGabrielQS <victor.git24@gmail.com>
2025-09-03 16:28:41 -0300

    Spring Boot

--- 3_Classes_e_Metodos/o_Desafio_Modulo/Desafio3/Main.java
+++ 3_Classes_e_Metodos/o_Desafio_Modulo/Desafio3/Main.java
@@ -3,6 +3,11 @@ package o_Desafio_Modulo.Desafio3;
 public class Main {
     public static void main(String[] args) {
 
+        ContaBancaria conta1 = new ContaBancaria();
+        conta1.depositar(20);
+        conta1.sacar(5);
+        conta1.mostrarSaldo();
+
 
     }
 }
