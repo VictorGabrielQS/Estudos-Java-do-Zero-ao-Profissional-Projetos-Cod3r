2b3fea4b52e9134d37e16e95bba191294655aa10
VictorGabrielQS <victor.git24@gmail.com>
2025-09-05 06:59:23 -0300

    Miniatura da aula 0:03 / 4:56 426. Passando Parâmetros para Web Service #01

---
+++ exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exercicios_sb/controller/MetodosHttpController.java
@@ -0,0 +1,44 @@
+package br.com.cod3r.exercicios_sb.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/metodos")
+public class MetodosHttpController {
+
+
+    @GetMapping
+    public String get(){
+        return  "Requisição GET";
+    }
+
+
+    @PostMapping
+    public String post(){
+        return  "Requisição POST";
+    }
+
+
+    @PutMapping
+    public String put(){
+        return  "Requisição PUT";
+    }
+
+
+    @PatchMapping
+    public String patch(){
+        return  "Requisição PATCH";
+    }
+
+
+    @DeleteMapping
+    public String delete(){
+        return  "Requisição DELETE";
+    }
+
+
+
+
+
+
+}
